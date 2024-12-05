696d210e93724b88303d037afcdb9a36b07d8af4
JesusVeraPompa <merkapompa@gmail.com>
2024-12-05 18:51:06 -0500

    1.3 Home PC listo

--- src/Layout/StandarLayout.jsx
+++ src/Layout/StandarLayout.jsx
@@ -1,4 +1,4 @@
-import NavBarComponents from '../Components/NavBarComponents'
+//import NavBarComponents from '../Components/NavBarComponents'
 import { Outlet } from 'react-router-dom'
 //import FooterComponents from '../Components/FooterComponents'
 import '../index.css'
