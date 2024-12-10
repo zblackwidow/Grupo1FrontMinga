efaa1b2ed341e6d1543de08a55ce7070e0cffb02
Black <alejandrazacariasrt@gmail.com>
2024-12-10 20:47:59 -0300

    logo navbar

--- src/Components/Login/Register.jsx
+++ src/Components/Login/Register.jsx
@@ -59,7 +59,7 @@ function Register() {
 
     return (
         <div className="w-full flex justify-center items-center font-poppins">
-            <div className="w-full my-16 md:w-[50%] flex justify-center">
+            <div className="w-full  md:w-[50%] flex justify-center">
                 <div className="md:w-[30vw] w-[90vw]">
                     <div className='flex flex-col items-center mt-36'>
                         <img src={logo} alt="logo" className="object-contain w-[127px]" />
@@ -105,11 +105,17 @@ function Register() {
                             Sign up
                         </button>
                     </form>
-                    <button 
-                        className="w-full mt-4 bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-700"
+                    <button
+                        type="button"
                         onClick={handleGoogleSignIn}
+                        className="w-full mt-4 flex p-3 justify-center text-black rounder-xl border-2 border-gray-300 bg-white rounded-xl"
                     >
-                        Sign in with Google
+                        <img
+                            src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+                            alt="Google logo"
+                            className="w-5 h-5 mr-2"
+                        />
+                          Sign in with Google
                     </button>
                     <div className="mt-4 text-center">
                         <p className="text-gray-500">
