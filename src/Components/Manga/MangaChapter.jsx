647e26a29b389e9e9058ed7aacd60d7843006042
andres manrique <manriqueandres1104@gmail.com>
2024-12-10 23:34:03 -0500

    chage chapter

--- src/Components/Manga/MangaChapter.jsx
+++ src/Components/Manga/MangaChapter.jsx
@@ -65,46 +65,77 @@ console.log(chapterList);
 </header>
 
 
-      {/* Visor de imagen */}
-      <div className="relative w-full max-w-3xl bg-black border border-gray-700 rounded overflow-hidden">
-        {pagesChapter && pagesChapter?.pages &&  pagesChapter.pages.length > 0 ? (
-          <>
-            <img
-              src={pagesChapter.pages[currentPage]}
-              alt={`Página ${currentPage + 1}`}
-              className="w-full h-auto"
-            />
-            <div className="absolute top-1/2 transform -translate-y-1/2 flex justify-between w-full px-4">
-              <button
-                onClick={goToPreviousPage}
-                disabled={isFirstPage}
-                className={`px-4 py-2 bg-transparent rounded ${
-                  isFirstPage ? "opacity-50 cursor-not-allowed" : "hover:bg-[#f97316]"
-                }`}
-              >
-                {/* Flecha izquierda */}
-                <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12.7485 9C13.3008 9 13.7485 8.55228 13.7485 8C13.7485 7.44772 13.3008 7 12.7485 7V9ZM0.292794 7.29289C-0.0977306 7.68342 -0.0977306 8.31658 0.292794 8.70711L6.65675 15.0711C7.04728 15.4616 7.68044 15.4616 8.07097 15.0711C8.46149 14.6805 8.46149 14.0474 8.07097 13.6569L2.41411 8L8.07097 2.34315C8.46149 1.95262 8.46149 1.31946 8.07097 0.928932C7.68044 0.538408 7.04728 0.538408 6.65675 0.928932L0.292794 7.29289ZM12.7485 7L0.999901 7V9L12.7485 9V7Z" fill="#424242"/>
-                </svg>
-              </button>
-              <button
-                onClick={goToNextPage}
-                disabled={isLastPage}
-                className={`px-4 py-2 bg-transparent rounded ${
-                  isLastPage ? "opacity-50 cursor-not-allowed" : "hover:bg-[#f97316]"
-                }`}
-              >
-                {/* Flecha derecha */}
-                <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M1.25146 9C0.69918 9 0.251465 8.55228 0.251465 8C0.251465 7.44772 0.69918 7 1.25146 7V9ZM13.7072 7.29289C14.0977 7.68342 14.0977 8.31658 13.7072 8.70711L7.34325 15.0711C6.95272 15.4616 6.31956 15.4616 5.92903 15.0711C5.53851 14.6805 5.53851 14.0474 5.92903 13.6569L11.5859 8L5.92903 2.34315C5.53851 1.95262 5.53851 1.31946 5.92903 0.928932C6.31956 0.538408 6.95272 0.538408 7.34325 0.928932L13.7072 7.29289ZM1.25146 7L13.0001 7V9L1.25146 9V7Z" fill="#424242"/>
-                </svg>
-              </button>
-            </div>
-          </>
-        ) : (
-          <p className="text-center p-4">No manga available</p>
-        )}
+<div className="relative w-full max-w-4xl bg-gray-400 border border-gray-700 rounded overflow-hidden py-4">
+  <div className=" w-auto flex justify-center ">
+  {pagesChapter && pagesChapter.pages && pagesChapter.pages.length > 0 ? (
+    <>
+      {/* Contenedor de la imagen */}
+      <div className="relative">
+        <img
+          src={pagesChapter.pages[currentPage]}
+          alt={`Página ${currentPage + 1}`}
+          className="w-full h-auto"
+        />
       </div>
+    </>
+  ) : (
+    <p className="text-center p-4">No manga available</p>
+  )}
+
+  </div>
+  
+  {/* Contenedor de las flechas */}
+  <div className="absolute inset-0 flex justify-between items-center pointer-events-none">
+    {/* Flecha izquierda */}
+    <div className="flex items-center justify-center pointer-events-auto ">
+      <button
+        onClick={goToPreviousPage}
+        disabled={isFirstPage}
+        className={`px-4 py-2 bg-transparent rounded ${
+          isFirstPage ? "opacity-50 cursor-not-allowed" : "hover:bg-transparent sm:hover:bg-[#f97316]"
+        }`}
+      >
+        <svg
+          width="28"
+          height="32"
+          viewBox="0 0 14 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12.7485 9C13.3008 9 13.7485 8.55228 13.7485 8C13.7485 7.44772 13.3008 7 12.7485 7V9ZM0.292794 7.29289C-0.0977306 7.68342 -0.0977306 8.31658 0.292794 8.70711L6.65675 15.0711C7.04728 15.4616 7.68044 15.4616 8.07097 15.0711C8.46149 14.6805 8.46149 14.0474 8.07097 13.6569L2.41411 8L8.07097 2.34315C8.46149 1.95262 8.46149 1.31946 8.07097 0.928932C7.68044 0.538408 7.04728 0.538408 6.65675 0.928932L0.292794 7.29289ZM12.7485 7L0.999901 7V9L12.7485 9V7Z"
+            fill="#424242"
+          />
+        </svg>
+      </button>
+    </div>
+
+    {/* Flecha derecha */}
+    <div className="flex items-center justify-center pointer-events-auto ">
+      <button
+        onClick={goToNextPage}
+        disabled={isLastPage}
+        className={`px-4 py-2 bg-transparent rounded ${
+          isLastPage ? "opacity-50 cursor-not-allowed" : "hover:bg-transparent  sm:hover:bg-[#f97316]"
+        }`}
+      >
+        <svg
+          width="28"
+          height="32"
+          viewBox="0 0 14 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M1.25146 9C0.69918 9 0.251465 8.55228 0.251465 8C0.251465 7.44772 0.69918 7 1.25146 7V9ZM13.7072 7.29289C14.0977 7.68342 14.0977 8.31658 13.7072 8.70711L7.34325 15.0711C6.95272 15.4616 6.31956 15.4616 5.92903 15.0711C5.53851 14.6805 5.53851 14.0474 5.92903 13.6569L11.5859 8L5.92903 2.34315C5.53851 1.95262 5.53851 1.31946 5.92903 0.928932C6.31956 0.538408 6.95272 0.538408 7.34325 0.928932L13.7072 7.29289ZM1.25146 7L13.0001 7V9L1.25146 9V7Z"
+            fill="#424242"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+</div>
+
 
       <div className="min-h-0 text-white flex items-center p-4">
         <div className="w-auto flex">
